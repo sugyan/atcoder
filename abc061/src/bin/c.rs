c0c6162d859fd3e5d4e64aa8f573bf293ffae322
sugyan <sugi1982@gmail.com>
2021-04-04 11:49:09 +0900

    Update abc061/c

--- abc061/src/bin/c.rs
+++ abc061/src/bin/c.rs
@@ -7,14 +7,12 @@ fn main() {
         mut ab: [(i32, usize); n],
     }
     ab.sort_unstable();
-    let mut answer = ab[0].0;
     let mut i = 0;
     for &(a, b) in &ab {
-        answer = a;
         i += b;
         if i >= k {
+            println!("{}", a);
             break;
         }
     }
-    println!("{}", answer);
 }
