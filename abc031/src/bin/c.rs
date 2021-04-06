112d7903cb8d4c89f384d187f58d62658c93cf78
sugyan <sugi1982@gmail.com>
2021-04-06 11:31:21 +0900

    Update abc031/c

--- abc031/src/bin/c.rs
+++ abc031/src/bin/c.rs
@@ -8,22 +8,25 @@ fn main() {
     }
     let mut answer = std::i32::MIN;
     for i in 0..n {
-        let results = (0..n)
-            .filter_map(|j| {
-                let mut points = (0, 0);
-                for (k, &m) in a[i.min(j)..=i.max(j)].iter().enumerate() {
-                    if k % 2 == 0 {
-                        points.0 += m;
-                    } else {
-                        points.1 += m;
-                    }
+        let (mut maxval, mut result) = (std::i32::MIN, 0);
+        for j in 0..n {
+            if j == i {
+                continue;
+            }
+            let mut points = (0, 0);
+            for (k, &m) in a[i.min(j)..=i.max(j)].iter().enumerate() {
+                if k % 2 == 0 {
+                    points.0 += m;
+                } else {
+                    points.1 += m;
                 }
-                Some(points).filter(|_| i != j)
-            })
-            .collect::<Vec<_>>();
-        if let Some(&max) = results.iter().max_by_key(|&p| p.1) {
-            answer = answer.max(results.iter().find(|&p| p.1 == max.1).unwrap().0);
+            }
+            if points.1 > maxval {
+                result = points.0;
+                maxval = points.1;
+            }
         }
+        answer = answer.max(result);
     }
     println!("{}", answer);
 }
