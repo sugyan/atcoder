073b3473edb4b3c24d702a525cb403baec0f3f0c
sugyan <sugi1982@gmail.com>
2021-04-07 22:53:01 +0900

    Update arc031/b

--- arc031/src/bin/b.rs
+++ arc031/src/bin/b.rs
@@ -1,50 +1,41 @@
 use proconio::marker::Chars;
 use proconio::{fastout, input};
-use std::collections::{HashSet, VecDeque};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
     input! {
         mut a: [Chars; 10],
     }
-    let mut candidates = Vec::new();
-    for i in 0..10 {
-        for j in 0..10 {
-            if a[i][j] == 'o' {
-                let mut hs = HashSet::new();
-                let mut vd = VecDeque::new();
-                vd.push_back((i, j));
-                while let Some((i, j)) = vd.pop_front() {
-                    for &(di, dj) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
-                        let (i, j) = (i as i32 + di, j as i32 + dj);
-                        if (0..10).contains(&i) && (0..10).contains(&j) {
-                            let (i, j) = (i as usize, j as usize);
-                            match a[i][j] {
-                                'o' => {
-                                    a[i][j] = '#';
-                                    vd.push_back((i, j));
-                                }
-                                'x' => {
-                                    hs.insert((i, j));
-                                }
-                                _ => {}
-                            }
-                        }
+    let check = |(i, j): (usize, usize)| -> bool {
+        let mut a = a.clone();
+        let mut vd = VecDeque::new();
+        vd.push_back((i, j));
+        while let Some((i, j)) = vd.pop_front() {
+            for &(di, dj) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                let (i, j) = (i as i32 + di, j as i32 + dj);
+                if (0..10).contains(&i) && (0..10).contains(&j) {
+                    let (i, j) = (i as usize, j as usize);
+                    if a[i][j] == 'o' {
+                        a[i][j] = 'x';
+                        vd.push_back((i, j));
                     }
                 }
-                candidates.push(hs);
             }
         }
-    }
-    let intersection = |acc: Option<HashSet<_>>, x: &HashSet<_>| {
-        Some(acc.map_or(x.clone(), |a| a.intersection(x).cloned().collect()))
+        a.iter().all(|row| row.iter().all(|&c| c == 'x'))
     };
-    if candidates
-        .iter()
-        .fold(None, intersection)
-        .filter(|s| !s.is_empty())
-        .is_some()
-    {
+    let can_unify = || -> bool {
+        for (i, row) in a.iter().enumerate() {
+            for (j, &col) in row.iter().enumerate() {
+                if col == 'x' && check((i, j)) {
+                    return true;
+                }
+            }
+        }
+        false
+    };
+    if can_unify() {
         println!("YES");
     } else {
         println!("NO");
