9950552ca5ae3ac8c6ac90adde83e2d056bd8ced
sugyan <sugi1982@gmail.com>
2021-04-16 09:51:57 +0900

    Update arc036/b

--- arc036/src/bin/b.rs
+++ arc036/src/bin/b.rs
@@ -6,23 +6,15 @@ fn main() {
         n: usize,
         h: [u32; n],
     }
-    let mut v = vec![0; n];
+    let mut v = vec![1; n];
     let mut l = 0;
     for i in 1..n {
-        if h[i] > h[i - 1] {
-            l += 1;
-        } else {
-            l = 0;
-        }
+        l = if h[i] > h[i - 1] { l + 1 } else { 0 };
         v[i] += l;
     }
     for i in (0..n - 1).rev() {
-        if h[i] > h[i + 1] {
-            l += 1;
-        } else {
-            l = 0;
-        }
+        l = if h[i] > h[i + 1] { l + 1 } else { 0 };
         v[i] += l;
     }
-    println!("{}", v.iter().max().unwrap() + 1);
+    println!("{}", v.iter().max().unwrap());
 }
