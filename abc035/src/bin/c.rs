aabf0c3c265eddcae4f99e2932f205cfa6bbed78
sugyan <sugi1982@gmail.com>
2021-04-20 23:22:16 +0900

    Update abc035/c

--- abc035/src/bin/c.rs
+++ abc035/src/bin/c.rs
@@ -1,24 +1,24 @@
+use proconio::marker::Usize1;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
         n: usize, q: usize,
-        lr: [(usize, usize); q],
+        lr: [(Usize1, usize); q],
     }
     let mut v = vec![false; n + 1];
     for &(l, r) in &lr {
-        v[l - 1] = !v[l - 1];
+        v[l] = !v[l];
         v[r] = !v[r];
     }
-    let answer = (0..n)
-        .scan(0, |state, i| {
-            if v[i] {
-                *state = 1 - *state;
-            }
-            Some(*state)
-        })
-        .map(|b| (b + b'0') as char)
-        .collect::<String>();
+    let mut answer = String::new();
+    let mut b = 0;
+    (0..n).for_each(|i| {
+        if v[i] {
+            b = 1 - b;
+        }
+        answer.push((b + b'0') as char);
+    });
     println!("{}", answer);
 }
