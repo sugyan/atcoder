7cb1b5145c40bb059ff542d6bb8bd79697740148
sugyan <sugi1982@gmail.com>
2021-04-22 11:26:21 +0900

    Update arc084/c

--- arc084/src/bin/c.rs
+++ arc084/src/bin/c.rs
@@ -11,25 +11,16 @@ fn main() {
     a.sort_unstable();
     b.sort_unstable();
     c.sort_unstable();
-    let mut bc = vec![0; n];
-    let (mut j, mut k) = (0, 0);
-    for (j, &b) in b.iter().enumerate() {
+    let mut answer = 0;
+    let (mut i, mut k) = (0, 0);
+    for &b in &b {
+        while i < n && a[i] < b {
+            i += 1;
+        }
         while k < n && c[k] <= b {
             k += 1;
         }
-        bc[j] += n - k;
-    }
-    for i in (0..n - 1).rev() {
-        bc[i] += bc[i + 1];
-    }
-    let mut answer = 0;
-    for &a in &a {
-        while j < n && b[j] <= a {
-            j += 1;
-        }
-        if j < n {
-            answer += bc[j];
-        }
+        answer += i * (n - k);
     }
     println!("{}", answer);
 }
