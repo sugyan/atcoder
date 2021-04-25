37776587da6bf3ce63057fe652a45d989d7951db
sugyan <sugi1982@gmail.com>
2021-04-25 22:49:45 +0900

    Update arc042/b
    https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points

--- arc042/src/bin/b.rs
+++ arc042/src/bin/b.rs
@@ -9,21 +9,11 @@ fn main() {
     }
     let mut answer = std::f64::MAX;
     for i in 0..n {
-        let dx = xy[i].0 - xy[(i + 1) % n].0;
-        let dy = xy[i].1 - xy[(i + 1) % n].1;
-        let d = match (dx == 0, dy == 0) {
-            (true, _) => (xy[i].0 - x).abs() as f64,
-            (_, true) => (xy[i].1 - y).abs() as f64,
-            _ => {
-                let a = dy as f64 / dx as f64;
-                let b = xy[i].1 as f64 - a * xy[i].0 as f64;
-                let c = y as f64 + x as f64 / a;
-                let cx = a * (c - b) / (a * a + 1.0);
-                let cy = (a * a * c + b) / (a * a + 1.0);
-                ((cx - x as f64).powf(2.0) + (cy - y as f64).powf(2.0)).sqrt()
-            }
-        };
-        answer = answer.min(d);
+        let dx = xy[(i + 1) % n].0 - xy[i].0;
+        let dy = xy[(i + 1) % n].1 - xy[i].1;
+        let ex = x - xy[i].0;
+        let ey = y - xy[i].1;
+        answer = answer.min((dx * ey - dy * ex).abs() as f64 / ((dx * dx + dy * dy) as f64).sqrt());
     }
     println!("{:.10}", answer);
 }
