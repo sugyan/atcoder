661aa09ee524fced5d4294f4805609abb89e76c5
sugyan <sugi1982@gmail.com>
2021-04-28 01:35:43 +0900

    Update abc080/c

--- abc080/src/bin/c.rs
+++ abc080/src/bin/c.rs
@@ -9,19 +9,14 @@ fn main() {
         f: [[u8; 10]; n],
         p: [[i64; 11]; n],
     }
-    let f = f
-        .iter()
-        .map(|v| {
-            v.iter()
-                .enumerate()
-                .map(|(i, &u)| if u == 1 { 1 << i } else { 0 })
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
     let mut answer = std::i64::MIN;
     for i in 1..1 << 10 {
         let sum = (0..n)
-            .map(|j| p[j][(f[j] & i).count_ones() as usize])
+            .map(|j| {
+                p[j][(0..10)
+                    .filter(|&k| f[j][k] > 0 && ((i >> k) & 1 > 0))
+                    .count()]
+            })
             .sum::<i64>();
         answer = answer.max(sum);
     }
