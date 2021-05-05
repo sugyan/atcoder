452aa3b6ac433069ee5d0653aa748f1b6c98f29a
sugyan <sugi1982@gmail.com>
2021-05-05 13:44:56 +0900

    Update abc194/e

--- abc194/src/bin/e.rs
+++ abc194/src/bin/e.rs
@@ -4,25 +4,17 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize, m: usize,
-        a: [u32; n],
+        a: [usize; n],
     }
-    let mut v = vec![None; n];
-    let mut prev = vec![0; n];
-    for (i, &a) in a.iter().enumerate() {
-        let d = i - prev[a as usize] + 1;
-        v[a as usize] = Some(v[a as usize].map_or(d, |v: usize| v.max(d)));
-        prev[a as usize] = i + 1;
+    let mut counts = vec![0; n];
+    (0..m).for_each(|i| counts[a[i]] += 1);
+    let mut answer = counts.iter().position(|&c| c == 0).unwrap_or(m);
+    for i in 0..n - m {
+        counts[a[i + m]] += 1;
+        counts[a[i]] -= 1;
+        if counts[a[i]] == 0 {
+            answer = answer.min(a[i]);
+        }
     }
-    for i in 0..n {
-        v[i] = v[i].map(|v| v.max(n + 1 - prev[i]));
-    }
-    println!(
-        "{}",
-        v.iter()
-            .position(|&e| match e {
-                Some(w) if w <= m => false,
-                _ => true,
-            })
-            .unwrap_or(n)
-    );
+    println!("{}", answer);
 }
