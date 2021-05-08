5c7702e31f4c430db9480dd684ba53743cd0f1a4
sugyan <sugi1982@gmail.com>
2021-05-08 12:53:03 +0900

    Update arc017/b

--- arc017/src/bin/b.rs
+++ arc017/src/bin/b.rs
@@ -7,20 +7,12 @@ fn main() {
         a: [u32; n],
     }
     let mut answer = 0;
-    if k > 1 {
-        let mut len = 0;
-        for i in 0..n - 1 {
-            if a[i] < a[i + 1] {
-                len += 1;
-                if len >= k - 1 {
-                    answer += 1;
-                }
-            } else {
-                len = 0;
-            }
+    let mut len = 1;
+    for i in 0..n - 1 {
+        len = if a[i] < a[i + 1] { len + 1 } else { 1 };
+        if len >= k {
+            answer += 1;
         }
-    } else {
-        answer = n;
     }
-    println!("{}", answer);
+    println!("{}", if k > 1 { answer } else { n });
 }
