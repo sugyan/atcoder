bc56c9a5e6b5f067eb31e8f80dac289e68a53368
sugyan <sugi1982@gmail.com>
2021-05-12 00:30:17 +0900

    Update abc121/d

--- abc121/src/bin/d.rs
+++ abc121/src/bin/d.rs
@@ -3,19 +3,8 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        mut a: u64, b: u64,
+        mut a: i64, b: i64,
     }
-    if a > 0 {
-        a -= 1;
-    }
-    let mut answer = 0;
-    for i in 0..64 {
-        let j = 1 << i;
-        let na = a / j / 2 * j + if (a / j) % 2 == 1 { a % j + 1 } else { 0 };
-        let nb = b / j / 2 * j + if (b / j) % 2 == 1 { b % j + 1 } else { 0 };
-        if (nb - na) % 2 == 1 {
-            answer += j;
-        }
-    }
-    println!("{}", answer);
+    let f = |n: i64| (n + 1) >> 1 & 1 ^ if n & 1 > 0 { 0 } else { n };
+    println!("{}", f(b) ^ f(a - 1));
 }
