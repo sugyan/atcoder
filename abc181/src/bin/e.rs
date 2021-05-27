633dd77d3bd8a6b1c0e9109c124d6dd6e361ba76
sugyan <sugi1982@gmail.com>
2021-05-28 01:23:25 +0900

    Update abc181/e

--- abc181/src/bin/e.rs
+++ abc181/src/bin/e.rs
@@ -4,27 +4,27 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize, m: usize,
-        mut h: [u32; n],
-        mut w: [u32; m],
+        mut h: [i32; n],
+        mut w: [i32; m],
     }
     h.sort_unstable();
-    w.sort_unstable();
-    let d = |x: u32| match w.binary_search(&x) {
-        Ok(_) => 0,
-        Err(i) => {
-            let mut ret = if i > 0 { x - w[i - 1] } else { w[0] - x };
-            if i < m {
-                ret = ret.min(w[i] - x);
-            }
-            ret
-        }
-    };
-    let mut sum = (0..n / 2).map(|i| h[i * 2 + 2] - h[i * 2 + 1]).sum::<u32>();
-    let mut answer = sum + d(h[0]);
+    let mut sum = (vec![0; n / 2 + 1], vec![0; n / 2 + 1]);
     for i in 0..n / 2 {
-        sum -= h[i * 2 + 2] - h[i * 2 + 1];
-        sum += h[i * 2 + 1] - h[i * 2];
-        answer = answer.min(sum + d(h[i * 2 + 2]));
+        sum.0[i + 1] = sum.0[i] + h[i * 2 + 1] - h[i * 2];
     }
+    for i in (0..n / 2).rev() {
+        sum.1[i] = sum.1[i + 1] + h[i * 2 + 2] - h[i * 2 + 1];
+    }
+    let answer = w
+        .iter()
+        .map(|w| {
+            let i = match h.binary_search(w) {
+                Ok(i) => i,
+                Err(i) => i,
+            } & !1;
+            sum.0[i / 2] + sum.1[i / 2] + (h[i] - w).abs()
+        })
+        .min()
+        .unwrap();
     println!("{}", answer);
 }
