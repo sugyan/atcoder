a2709cb5ec065f5aa9c73c79796790b241b94d5a
sugyan <sugi1982@gmail.com>
2021-06-04 23:00:43 +0900

    Update apc001/b

--- apc001/src/bin/b.rs
+++ apc001/src/bin/b.rs
@@ -1,20 +1,15 @@
 use proconio::{fastout, input};
-use std::cmp::Ordering;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        mut a: [i64; n],
-        mut b: [i64; n],
+        a: [i64; n],
+        b: [i64; n],
     }
-    let mut sum = 0;
-    for i in 0..n {
-        match a[i].cmp(&b[i]) {
-            Ordering::Less => sum -= (b[i] - a[i]) / 2,
-            Ordering::Equal => {}
-            Ordering::Greater => sum += a[i] - b[i],
-        }
-    }
-    println!("{}", if sum > 0 { "No" } else { "Yes" });
+    let t = (0..n)
+        .map(|i| a[i] - b[i])
+        .map(|d| if d < 0 { d / 2 } else { d })
+        .sum::<i64>();
+    println!("{}", if t > 0 { "No" } else { "Yes" });
 }
