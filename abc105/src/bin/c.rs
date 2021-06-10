c9f79a27d852d1cd778110fbe729203a26a0f3e6
sugyan <sugi1982@gmail.com>
2021-06-11 00:50:12 +0900

    Update abc105/c

--- abc105/src/bin/c.rs
+++ abc105/src/bin/c.rs
@@ -7,16 +7,9 @@ fn main() {
     }
     let mut s = Vec::new();
     while n != 0 {
-        s.push(if n % 2 != 0 {
-            n -= 1;
-            '1'
-        } else {
-            '0'
-        });
-        n /= -2;
+        s.push(if n % 2 != 0 { '1' } else { '0' });
+        n = (n - (n & 1)) / -2;
     }
-    if s.is_empty() {
-        s.push('0');
-    }
-    println!("{}", s.iter().rev().collect::<String>());
+    let answer = s.iter().rev().collect::<String>();
+    println!("{}", if answer.is_empty() { "0" } else { &answer });
 }
