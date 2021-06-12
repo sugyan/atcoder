391a8631a8da5b3c4e181364b2fbfff5975e94be
sugyan <sugi1982@gmail.com>
2021-06-12 22:53:15 +0900

    Update abc176/e

--- abc176/src/bin/e.rs
+++ abc176/src/bin/e.rs
@@ -1,5 +1,4 @@
 use proconio::{fastout, input};
-use std::collections::HashSet;
 
 #[fastout]
 fn main() {
@@ -8,34 +7,16 @@ fn main() {
         hw: [(usize, usize); m],
     }
     let (mut hc, mut wc) = (vec![0; h], vec![0; w]);
-    let (mut hmax, mut wmax) = (0, 0);
-    let (mut hv, mut wv) = (Vec::new(), Vec::new());
     for &(h, w) in &hw {
         hc[h - 1] += 1;
         wc[w - 1] += 1;
-        if hc[h - 1] >= hmax {
-            if hc[h - 1] > hmax {
-                hmax = hc[h - 1];
-                hv.clear();
-            }
-            hv.push(h - 1);
-        }
-        if wc[w - 1] >= wmax {
-            if wc[w - 1] > wmax {
-                wmax = wc[w - 1];
-                wv.clear();
-            }
-            wv.push(w - 1);
-        }
     }
-    let hw = hw.iter().collect::<HashSet<_>>();
-    let mut answer = hc[hv[0]] + wc[wv[0]];
-    if hv
+    let hmax = *hc.iter().max().unwrap();
+    let wmax = *wc.iter().max().unwrap();
+    let dup = hw
         .iter()
-        .flat_map(|i| std::iter::repeat(i).zip(&wv))
-        .all(|(&i, &j)| hw.contains(&(i + 1, j + 1)))
-    {
-        answer -= 1;
-    }
-    println!("{}", answer);
+        .filter(|&(h, w)| hc[h - 1] == hmax && wc[w - 1] == wmax)
+        .count()
+        >= hc.iter().filter(|&c| *c == hmax).count() * wc.iter().filter(|&c| *c == wmax).count();
+    println!("{}", hmax + wmax - if dup { 1 } else { 0 });
 }
