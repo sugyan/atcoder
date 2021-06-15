111d1261030759d5cd0831ee445b45076152052a
sugyan <sugi1982@gmail.com>
2021-06-15 22:23:13 +0900

    Update abc157/d

--- abc157/src/bin/d.rs
+++ abc157/src/bin/d.rs
@@ -1,5 +1,5 @@
+use petgraph::unionfind::UnionFind;
 use proconio::{fastout, input};
-use std::collections::{HashSet, VecDeque};
 
 #[fastout]
 fn main() {
@@ -8,44 +8,27 @@ fn main() {
         ab: [(usize, usize); m],
         cd: [(usize, usize); k],
     }
-    let mut graph = vec![Vec::new(); n];
-    let mut block = vec![HashSet::new(); n];
+    let mut v = vec![-1; n];
+    let mut uf = UnionFind::new(n);
     for &(a, b) in &ab {
-        graph[a - 1].push(b - 1);
-        graph[b - 1].push(a - 1);
+        v[a - 1] -= 1;
+        v[b - 1] -= 1;
+        uf.union(a - 1, b - 1);
     }
     for &(c, d) in &cd {
-        block[c - 1].insert(d - 1);
-        block[d - 1].insert(c - 1);
-    }
-    let mut seen = vec![false; n];
-    let mut answers = vec![0; n];
-    for i in 0..n {
-        if seen[i] {
-            continue;
-        }
-        let mut hs = HashSet::new();
-        let mut vd = VecDeque::new();
-        vd.push_back(i);
-        while let Some(i) = vd.pop_front() {
-            hs.insert(i);
-            for &j in &graph[i] {
-                if !seen[j] {
-                    seen[j] = true;
-                    vd.push_back(j);
-                }
-            }
-        }
-        for &j in &hs {
-            answers[j] = hs.len() - graph[j].len() - 1 - block[j].intersection(&hs).count();
+        if uf.equiv(c - 1, d - 1) {
+            v[c - 1] -= 1;
+            v[d - 1] -= 1;
         }
     }
-    println!(
-        "{}",
-        answers
-            .iter()
-            .map(|i| i.to_string())
-            .collect::<Vec<_>>()
-            .join(" ")
-    );
+    let counts = (0..n).fold(vec![0; n], |mut acc, x| {
+        acc[uf.find(x)] += 1;
+        acc
+    });
+    let answers = v
+        .iter()
+        .enumerate()
+        .map(|(i, &e)| (e + counts[uf.find(i)]).to_string())
+        .collect::<Vec<_>>();
+    println!("{}", answers.join(" "));
 }
