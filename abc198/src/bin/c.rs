f2d18f25953e8bc6fffabc55e7d93a2047cf8815
sugyan <sugi1982@gmail.com>
2021-06-17 23:07:58 +0900

    Update abc198/c

--- abc198/src/bin/c.rs
+++ abc198/src/bin/c.rs
@@ -3,16 +3,8 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        r: u64, x: u64, y: u64,
+        r: f64, x: f64, y: f64,
     }
-    let d = ((x * x + y * y) as f64).sqrt().ceil() as u64;
-    let answer = (d - 1) / r + 1;
-    println!(
-        "{}",
-        if answer == 1 && r != d {
-            answer + 1
-        } else {
-            answer
-        }
-    );
+    let d = (x * x + y * y).sqrt();
+    println!("{}", if r > d { 2 } else { (d / r).ceil() as u32 });
 }
