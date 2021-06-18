783eb063fe8d422457ffc4de14e793c848445666
sugyan <sugi1982@gmail.com>
2021-06-18 22:39:41 +0900

    Update abc198/e

--- abc198/src/bin/e.rs
+++ abc198/src/bin/e.rs
@@ -14,29 +14,20 @@ fn main() {
     }
     let mut cs = vec![0; *c.iter().max().unwrap()];
     let mut answers = vec![true; n];
-    dfs(&graph, 0, None, &c, &mut cs, &mut answers);
-    for (i, &b) in answers.iter().enumerate() {
-        if b {
-            println!("{}", i + 1);
-        }
+    dfs(&graph, 0, n, &c, &mut cs, &mut answers);
+    for (i, _) in answers.iter().enumerate().filter(|(_, &b)| b) {
+        println!("{}", i + 1);
     }
 }
 
-fn dfs(
-    graph: &[Vec<usize>],
-    i: usize,
-    prev: Option<usize>,
-    c: &[usize],
-    cs: &mut Vec<usize>,
-    answers: &mut Vec<bool>,
-) {
+fn dfs(g: &[Vec<usize>], i: usize, p: usize, c: &[usize], cs: &mut [usize], answers: &mut [bool]) {
     if cs[c[i] - 1] > 0 {
         answers[i] = false;
     }
     cs[c[i] - 1] += 1;
-    for &j in &graph[i] {
-        if prev != Some(j) {
-            dfs(graph, j, Some(i), c, cs, answers);
+    for &j in &g[i] {
+        if p != j {
+            dfs(g, j, i, c, cs, answers);
         }
     }
     cs[c[i] - 1] -= 1;
