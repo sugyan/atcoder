3790d42de94e3919c5b36687406c89c3d9ce1cc5
sugyan <sugi1982@gmail.com>
2021-06-21 22:59:06 +0900

    Update arc107/b

--- arc107/src/bin/b.rs
+++ arc107/src/bin/b.rs
@@ -5,12 +5,8 @@ fn main() {
     input! {
         n: i64, k: i64,
     }
-    let mut answer = 0;
+    let k = k.abs();
     let m = |i: i64| n - (n + 1 - i).abs();
-    for i in 2..=(n * 2) {
-        if (k + 2..=n * 2 + k).contains(&i) {
-            answer += m(i) * m(i - k);
-        }
-    }
+    let answer = (k + 2..=n * 2).map(|x| m(x) * m(x - k)).sum::<i64>();
     println!("{}", answer);
 }
