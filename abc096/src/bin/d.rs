ae21a90e2c78e2dc1a9c5354647a80df1e322b20
sugyan <sugi1982@gmail.com>
2021-06-26 23:41:39 +0900

    Update abc096/d

--- abc096/src/bin/d.rs
+++ abc096/src/bin/d.rs
@@ -5,26 +5,13 @@ fn main() {
     input! {
         n: usize,
     }
-    let mut p = vec![true; 55_555];
-    p[0] = false;
-    p[1] = false;
-    for i in 2..55_555 >> 1 {
-        (i << 1..)
-            .step_by(i)
-            .take_while(|&j| j < 55_555)
-            .for_each(|j| p[j] = false);
+    let mut v = Vec::with_capacity(n);
+    let mut i = 1;
+    while v.len() < n {
+        i += 10;
+        if (2..i >> 1).all(|j| i % j != 0) {
+            v.push(i.to_string());
+        }
     }
-    let mod1 = p
-        .iter()
-        .enumerate()
-        .filter_map(|(i, &b)| if i % 5 == 1 && b { Some(i) } else { None })
-        .collect::<Vec<_>>();
-    println!(
-        "{}",
-        &mod1[..n]
-            .iter()
-            .map(usize::to_string)
-            .collect::<Vec<_>>()
-            .join(" ")
-    );
+    println!("{}", v.join(" "));
 }
