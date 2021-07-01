aea06af80d4e10bb502fb4c09e8e0af37776f7cd
sugyan <sugi1982@gmail.com>
2021-07-01 23:12:06 +0900

    Update abc174/d

--- abc174/src/bin/d.rs
+++ abc174/src/bin/d.rs
@@ -4,23 +4,9 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        _n: usize,
         s: Chars,
     }
-    let mut answer = 0;
-    let (mut lo, mut hi) = (0, n - 1);
-    while lo < hi {
-        while lo < hi && s[lo] == 'R' {
-            lo += 1;
-        }
-        while lo < hi && s[hi] == 'W' {
-            hi -= 1;
-        }
-        if lo < hi {
-            answer += 1;
-            lo += 1;
-            hi -= 1;
-        }
-    }
-    println!("{}", answer);
+    let r = s.iter().filter(|&c| *c == 'R').count();
+    println!("{}", (0..r).filter(|&i| s[i] == 'W').count());
 }
