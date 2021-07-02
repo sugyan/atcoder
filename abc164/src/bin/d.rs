0caa2c73452dd7c07d53727c564a8fce3232e577
sugyan <sugi1982@gmail.com>
2021-07-03 01:07:02 +0900

    Update abc164/d

--- abc164/src/bin/d.rs
+++ abc164/src/bin/d.rs
@@ -5,16 +5,14 @@ fn main() {
     input! {
         s: String,
     }
-    let mut v = vec![0; 2019];
-    let mut answer = 0;
-    for &b in s.as_bytes() {
-        let mut tmp = vec![0; 2019];
-        tmp[(b - b'0') as usize] += 1;
-        for i in 0..2019 {
-            tmp[(i * 10 + (b - b'0') as usize) % 2019] += v[i];
-        }
-        v = tmp;
-        answer += v[0];
+    let mut c = vec![0; 2019];
+    c[0] = 1;
+    let (mut n, mut p) = (0, 1);
+    for &b in s.as_bytes().iter().rev() {
+        n = (p * (b - b'0') as i32 + n) % 2019;
+        c[n as usize] += 1;
+        p = p * 10 % 2019;
     }
+    let answer = c.iter().map(|m| m * (m - 1) / 2).sum::<i32>();
     println!("{}", answer);
 }
