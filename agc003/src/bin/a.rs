bd8cf5a7a20d13cb8ac5e93765d9ac480de18c33
sugyan <sugi1982@gmail.com>
2021-07-04 00:44:58 +0900

    Update agc003/a

--- agc003/src/bin/a.rs
+++ agc003/src/bin/a.rs
@@ -5,17 +5,6 @@ fn main() {
     input! {
         s: String,
     }
-    let mut news = [0; 4];
-    s.chars().for_each(|c| {
-        news[match c {
-            'N' => 0,
-            'E' => 1,
-            'W' => 2,
-            'S' => 3,
-            _ => unreachable!(),
-        }] += 1
-    });
-    let ok = !(news[0] + news[3] > 0 && news[0] * news[3] == 0)
-        && !(news[1] + news[2] > 0 && news[1] * news[2] == 0);
-    println!("{}", if ok { "Yes" } else { "No" });
+    let no = s.contains('N') ^ s.contains('S') || s.contains('E') ^ s.contains('W');
+    println!("{}", if !no { "Yes" } else { "No" });
 }
