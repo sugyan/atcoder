b4b7dff8f8d4e33bbc7233558b37f2fc7bd6836b
sugyan <sugi1982@gmail.com>
2021-07-05 22:25:09 +0900

    Update abc089/d

--- abc089/src/bin/d.rs
+++ abc089/src/bin/d.rs
@@ -1,5 +1,4 @@
 use proconio::{fastout, input};
-use std::collections::HashMap;
 
 #[fastout]
 fn main() {
@@ -9,19 +8,17 @@ fn main() {
         q: usize,
         lr: [(usize, usize); q],
     }
-    let mut hm = HashMap::new();
+    let mut m = vec![(0, 0); h * w];
     for i in 0..h {
         for j in 0..w {
-            hm.insert(a[i][j], (i as isize, j as isize));
+            m[a[i][j] - 1] = (i as isize, j as isize);
         }
     }
     let mut v = vec![0; h * w];
     for i in 0..h * w - d {
-        if let (Some(p), Some(q)) = (hm.get(&(i + 1)), hm.get(&(i + 1 + d))) {
-            v[i + d] = v[i] + (p.0 - q.0).abs() + (p.1 - q.1).abs();
-        }
+        let (p, q) = (m[i], m[i + d]);
+        v[i + d] = v[i] + (p.0 - q.0).abs() + (p.1 - q.1).abs();
     }
-    // println!("{:?}", v);
     for &(l, r) in &lr {
         println!("{}", v[r - 1] - v[l - 1]);
     }
