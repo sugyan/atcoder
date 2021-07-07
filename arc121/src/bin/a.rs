090965de1a2310c98b53ecff72e43dafa7ee8642
sugyan <sugi1982@gmail.com>
2021-07-08 00:06:23 +0900

    Update arc121/a

--- arc121/src/bin/a.rs
+++ arc121/src/bin/a.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
@@ -6,32 +7,22 @@ fn main() {
         n: usize,
         xy: [(i32, i32); n],
     }
-    let mut x = xy
-        .iter()
-        .enumerate()
-        .map(|(i, &(x, _))| (x, i))
-        .collect::<Vec<_>>();
-    let mut y = xy
-        .iter()
-        .enumerate()
-        .map(|(i, &(_, y))| (y, i))
-        .collect::<Vec<_>>();
-    x.sort();
-    y.sort();
-    let mut v = vec![
-        (x[0].1, x[n - 1].1),
-        (x[1].1, x[n - 1].1),
-        (x[0].1, x[n - 2].1),
-        (y[0].1, y[n - 1].1),
-        (y[1].1, y[n - 1].1),
-        (y[0].1, y[n - 2].1),
-    ];
-    v.sort();
+    let mut idx = (0..n).collect::<Vec<_>>();
+    let mut v = Vec::<usize>::new();
+    idx.sort_by_cached_key(|&i| xy[i].0);
+    v.extend(&[idx[0], idx[1], idx[n - 1], idx[n - 2]]);
+    idx.sort_by_cached_key(|&i| xy[i].1);
+    v.extend(&[idx[0], idx[1], idx[n - 1], idx[n - 2]]);
+    v.sort_unstable();
     v.dedup();
-    let mut res = v
+    let mut d = v
         .iter()
-        .map(|&(i, j)| (xy[i].0 - xy[j].0).abs().max((xy[i].1 - xy[j].1).abs()))
+        .combinations(2)
+        .map(|v| {
+            let (&i, &j) = (v[0], v[1]);
+            (xy[i].0 - xy[j].0).abs().max((xy[i].1 - xy[j].1).abs())
+        })
         .collect::<Vec<_>>();
-    res.sort_unstable();
-    println!("{}", res[res.len() - 2]);
+    d.sort_unstable();
+    println!("{}", d[d.len() - 2]);
 }
