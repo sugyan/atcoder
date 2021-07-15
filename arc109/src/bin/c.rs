1bc7ceb6750762771a52dd04a000b5a8af3ec4e8
sugyan <sugi1982@gmail.com>
2021-07-16 00:09:12 +0900

    Update arc109/c

--- arc109/src/bin/c.rs
+++ arc109/src/bin/c.rs
@@ -1,33 +1,21 @@
+use proconio::marker::Chars;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
         _n: usize, k: usize,
-        s: String,
+        mut s: Chars,
     }
-    let mut v = s
-        .as_bytes()
-        .iter()
-        .map(|&u| match u {
-            b'R' => 0,
-            b'P' => 1,
-            b'S' => 2,
-            _ => unreachable!(),
-        })
-        .collect::<Vec<_>>();
     for _ in 0..k {
-        v = v
-            .repeat(v.len() % 2 + 1)
+        s = s
+            .repeat(2)
             .chunks(2)
-            .map(|w| {
-                if w[0] == w[1] {
-                    w[0]
-                } else {
-                    w[(3 + w[0] - w[1]) % 3 - 1]
-                }
+            .map(|c| match (c[0], c[1]) {
+                ('R', 'P') | ('P', 'S') | ('S', 'R') => c[1],
+                _ => c[0],
             })
             .collect();
     }
-    println!("{}", ['R', 'P', 'S'][v[0]]);
+    println!("{}", s[0]);
 }
