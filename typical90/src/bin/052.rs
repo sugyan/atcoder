92a9ad614a7b251d10e9a5a702b34ab0667303d7
sugyan <sugi1982@gmail.com>
2021-07-16 00:23:46 +0900

    Update typical90/052

--- typical90/src/bin/052.rs
+++ typical90/src/bin/052.rs
@@ -8,12 +8,6 @@ fn main() {
         n: usize,
         a: [[u64; 6]; n],
     }
-    let mut v = a[0].clone();
-    for a in a.iter().skip(1) {
-        v = a
-            .iter()
-            .map(|a| v.iter().map(|v| v * a).sum::<u64>() % MOD)
-            .collect();
-    }
-    println!("{}", v.iter().sum::<u64>() % MOD);
+    let answer = a.iter().fold(1, |acc, v| acc * v.iter().sum::<u64>() % MOD);
+    println!("{}", answer);
 }
