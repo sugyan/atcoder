79d70acec940fc984f05bbb0ad7d7b17b66d335b
sugyan <sugi1982@gmail.com>
2021-07-28 17:24:08 +0900

    Update abc180/e

--- abc180/src/bin/e.rs
+++ abc180/src/bin/e.rs
@@ -6,27 +6,20 @@ fn main() {
         n: usize,
         xyz: [(i32, i32, i32); n],
     }
-    let mut costs = vec![vec![0; n]; n];
-    for i in 0..n {
-        for j in 0..n {
-            costs[i][j] = (xyz[j].0 - xyz[i].0).abs()
-                + (xyz[j].1 - xyz[i].1).abs()
-                + 0.max(xyz[j].2 - xyz[i].2);
-        }
-    }
-    let mut dp = vec![vec![None; n]; 1 << n];
-    dp[0][0] = Some(0);
+    let mut dp = vec![vec![std::i32::MAX; n]; 1 << n];
+    dp[0][0] = 0;
     for i in 0..1 << n {
         for j in 0..n {
-            if i >> j & 1 != 0 {
-                for k in 0..n {
-                    if let Some(m) = dp[i - (1 << j)][k] {
-                        dp[i][j] =
-                            Some(dp[i][j].map_or(m + costs[k][j], |min| min.min(m + costs[k][j])));
-                    }
-                }
+            if i >> j & 1 == 0 {
+                continue;
+            }
+            for k in 0..n {
+                let c = (xyz[k].0 - xyz[j].0).abs()
+                    + (xyz[k].1 - xyz[j].1).abs()
+                    + 0.max(xyz[k].2 - xyz[j].2);
+                dp[i][j] = dp[i][j].min(dp[i - (1 << j)][k].saturating_add(c));
             }
         }
     }
-    println!("{}", dp[(1 << n) - 1][0].unwrap());
+    println!("{}", dp[(1 << n) - 1][0]);
 }
