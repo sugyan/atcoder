3419086cab3b010a19d78c5469e8741966062727
sugyan <sugi1982@gmail.com>
2021-07-29 23:08:31 +0900

    Update arc062/c

--- arc062/src/bin/c.rs
+++ arc062/src/bin/c.rs
@@ -7,14 +7,9 @@ fn main() {
         mut ta: [(u64, u64); n],
     }
     let mut m = (1, 1);
-    for (t, a) in ta.iter_mut() {
-        if *t < m.0 || *a < m.1 {
-            let f = (m.0 / *t + if m.0 % *t == 0 { 0 } else { 1 })
-                .max(m.1 / *a + if m.1 % *a == 0 { 0 } else { 1 });
-            *t *= f;
-            *a *= f;
-        }
-        m = (*t, *a);
+    for &(t, a) in &ta {
+        let f = ((m.0 + t - 1) / t).max((m.1 + a - 1) / a);
+        m = (t * f, a * f);
     }
     println!("{}", m.0 + m.1);
 }
