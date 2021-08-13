4540d1c1176c7292c53c34ace06b0156427bd825
sugyan <sugi1982@gmail.com>
2021-08-14 00:05:06 +0900

    Update abc075/c

--- abc075/src/bin/c.rs
+++ abc075/src/bin/c.rs
@@ -1,3 +1,4 @@
+use petgraph::unionfind::UnionFind;
 use proconio::{fastout, input};
 
 #[fastout]
@@ -6,30 +7,15 @@ fn main() {
         n: usize, m: usize,
         ab: [(usize, usize); m],
     }
-    let mut graph = vec![Vec::new(); n];
-    for &(a, b) in &ab {
-        graph[a - 1].push(b - 1);
-        graph[b - 1].push(a - 1);
-    }
     let mut answer = 0;
-    for e in &ab {
-        let mut visited = vec![false; n];
-        dfs(&graph, 0, e, &mut visited);
-        if !visited.iter().all(|&b| b) {
+    for i in 0..m {
+        let mut uf = UnionFind::new(n);
+        for j in (0..m).filter(|&j| j != i) {
+            uf.union(ab[j].0 - 1, ab[j].1 - 1);
+        }
+        if !(1..n).all(|j| uf.equiv(0, j)) {
             answer += 1;
         }
     }
     println!("{}", answer);
-}
-
-fn dfs(graph: &[Vec<usize>], i: usize, e: &(usize, usize), visited: &mut Vec<bool>) {
-    visited[i] = true;
-    for &j in &graph[i] {
-        if e == &(i + 1, j + 1) || e == &(j + 1, i + 1) {
-            continue;
-        }
-        if !visited[j] {
-            dfs(graph, j, e, visited);
-        }
-    }
 }
