3d1d034f1694b8d6980cf7777d73507959a63b8a
sugyan <sugi1982@gmail.com>
2021-08-24 01:18:10 +0900

    Update abc154/e

--- abc154/src/bin/e.rs
+++ abc154/src/bin/e.rs
@@ -7,28 +7,20 @@ fn main() {
         k: usize,
     }
     let n = n.bytes().map(|u| (u - b'0') as u32).collect::<Vec<_>>();
-    let mut dp = vec![vec![0; 100]; 3];
-    dp[0][0] = 9;
-    for i in 1..100 {
-        for j in 0..3 {
-            dp[j][i] = dp[j][i - 1] + 9 * if j > 0 { dp[j - 1][i - 1] } else { 1 };
+    let mut dp = vec![vec![vec![0; 2]; k + 2]; n.len() + 1];
+    dp[0][0][0] = 1;
+    for i in 0..n.len() {
+        for j in 0..=k {
+            for d in 0..10 {
+                let jj = j + if d != 0 { 1 } else { 0 };
+                for l in 0..2 {
+                    if n[i] < d && l == 0 {
+                        continue;
+                    }
+                    dp[i + 1][jj][if d < n[i] { 1 } else { l }] += dp[i][j][l];
+                }
+            }
         }
     }
-    println!("{}", helper(&dp, &n, k));
-}
-
-fn helper(dp: &[Vec<u32>], n: &[u32], k: usize) -> u32 {
-    if n.is_empty() {
-        return 0;
-    }
-    if n[0] == 0 {
-        return helper(dp, &n[1..], k);
-    }
-    if k == 1 {
-        return n[0] + (n.len() as u32 - 1) * 9;
-    }
-    if n.len() < 2 {
-        return 0;
-    }
-    dp[k - 1][n.len() - 2] + (n[0].max(1) - 1) * dp[k - 2][n.len() - 2] + helper(dp, &n[1..], k - 1)
+    println!("{}", dp[n.len()][k][0] + dp[n.len()][k][1]);
 }
