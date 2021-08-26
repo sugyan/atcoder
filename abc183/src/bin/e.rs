fa42a68a61cd892b669c4e885a96c023363e90a0
sugyan <sugi1982@gmail.com>
2021-08-26 23:05:07 +0900

    Update abc183/e

--- abc183/src/bin/e.rs
+++ abc183/src/bin/e.rs
@@ -9,34 +9,22 @@ fn main() {
         h: usize, w: usize,
         s: [Chars; h],
     }
-    let mut v = vec![vec![(0, 0, 0, 0); w]; h];
-    v[0][0] = (1, 1, 1, 1);
+    let mut v = vec![vec![(1, 0, 0, 0); w]; h];
     for i in 0..h {
         for j in 0..w {
-            if i == 0 && j == 0 {
+            if (i == 0 && j == 0) || s[i][j] == '#' {
                 continue;
             }
-            if s[i][j] == '.' {
-                v[i][j].0 += if i > 0 { v[i - 1][j].1 } else { 0 };
-                v[i][j].0 += if j > 0 { v[i][j - 1].2 } else { 0 };
-                v[i][j].0 += if i > 0 && j > 0 { v[i - 1][j - 1].3 } else { 0 };
-                v[i][j].1 += v[i][j].0;
-                v[i][j].2 += v[i][j].0;
-                v[i][j].3 += v[i][j].0;
-                if i > 0 && s[i - 1][j] == '.' {
-                    v[i][j].1 += v[i - 1][j].1;
-                }
-                if j > 0 && s[i][j - 1] == '.' {
-                    v[i][j].2 += v[i][j - 1].2;
-                }
-                if i > 0 && j > 0 && s[i - 1][j - 1] == '.' {
-                    v[i][j].3 += v[i - 1][j - 1].3;
-                }
-                v[i][j].0 %= MOD;
-                v[i][j].1 %= MOD;
-                v[i][j].2 %= MOD;
-                v[i][j].3 %= MOD;
+            if i > 0 && s[i - 1][j] == '.' {
+                v[i][j].1 = (v[i - 1][j].1 + v[i - 1][j].0) % MOD;
             }
+            if j > 0 && s[i][j - 1] == '.' {
+                v[i][j].2 = (v[i][j - 1].2 + v[i][j - 1].0) % MOD;
+            }
+            if i > 0 && j > 0 && s[i - 1][j - 1] == '.' {
+                v[i][j].3 = (v[i - 1][j - 1].3 + v[i - 1][j - 1].0) % MOD;
+            }
+            v[i][j].0 = (v[i][j].1 + v[i][j].2 + v[i][j].3) % MOD;
         }
     }
     println!("{}", v[h - 1][w - 1].0);
