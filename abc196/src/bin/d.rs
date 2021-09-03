519805f59c1a58e0eb1c41bf42ed26b6b30e68aa
sugyan <sugi1982@gmail.com>
2021-09-03 23:20:35 +0900

    Update abc196/d

--- abc196/src/bin/d.rs
+++ abc196/src/bin/d.rs
@@ -5,30 +5,25 @@ fn main() {
     input! {
         h: usize, w: usize, a: usize, _b: usize,
     }
-    let mut answer = 0;
-    backtrack(a, w, &mut vec![false; h * w], 0, &mut answer);
-    println!("{}", answer)
+    println!("{}", backtrack(&mut vec![false; h * w], w, a, 0));
 }
 
-fn backtrack(a: usize, w: usize, v: &mut Vec<bool>, p: usize, answer: &mut u32) {
+fn backtrack(v: &mut Vec<bool>, w: usize, a: usize, p: usize) -> i32 {
     if a == 0 {
-        *answer += 1;
-        return;
+        return 1;
     }
+    let mut ret = 0;
     for i in p..v.len() {
         if v[i] {
             continue;
         }
-        let mut js = vec![1, w];
-        js.dedup();
-        for &j in &js {
+        for j in if w == 1 { vec![1] } else { vec![1, w] } {
             if i + j < v.len() && !v[i + j] && (j == w || i / w == (i + 1) / w) {
-                v[i] = true;
                 v[i + j] = true;
-                backtrack(a - 1, w, v, i + 1, answer);
-                v[i] = false;
+                ret += backtrack(v, w, a - 1, i + 1);
                 v[i + j] = false;
             }
         }
     }
+    ret
 }
