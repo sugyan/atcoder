873283289c5dfbed9fe97a4506e88e6c923c2d93
sugyan <sugi1982@gmail.com>
2021-09-05 22:32:44 +0900

    Update abc184/d

--- abc184/src/bin/d.rs
+++ abc184/src/bin/d.rs
@@ -6,21 +6,16 @@ fn main() {
         a: usize, b: usize, c: usize,
     }
     let mut dp = vec![vec![vec![0.0; 101]; 101]; 101];
-    for i in (0..300).rev() {
-        for j in (0..=99.min(i)).rev() {
-            for k in (0..=99.min(i - j)).rev() {
-                let l = i - j - k;
-                if l > 99 {
-                    continue;
-                }
-                dp[j][k][l] += (1.0 + dp[j + 1][k][l]) * j as f64 / i as f64;
-                dp[j][k][l] += (1.0 + dp[j][k + 1][l]) * k as f64 / i as f64;
-                dp[j][k][l] += (1.0 + dp[j][k][l + 1]) * l as f64 / i as f64;
-                if j == a && k == b && l == c {
-                    println!("{:.10}", dp[j][k][l]);
-                    return;
-                }
+    for i in (0..100).rev() {
+        for j in (0..100).rev() {
+            for k in (0..100).rev() {
+                let sum = (i + j + k) as f64;
+                dp[i][j][k] = 1.0
+                    + dp[i + 1][j][k] * i as f64 / sum
+                    + dp[i][j + 1][k] * j as f64 / sum
+                    + dp[i][j][k + 1] * k as f64 / sum;
             }
         }
     }
+    println!("{:.10}", dp[a][b][c]);
 }
