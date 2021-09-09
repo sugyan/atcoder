0fd17fa65944d6309a30000c2a4e6d0b064f2d54
sugyan <sugi1982@gmail.com>
2021-09-09 22:59:57 +0900

    Update abc106/d

--- abc106/src/bin/d.rs
+++ abc106/src/bin/d.rs
@@ -4,22 +4,15 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize, m: usize, q: usize,
-        mut lr: [(usize, usize); m],
+        lr: [(usize, usize); m],
         pq: [(usize, usize); q],
     }
-    let mut v = vec![Vec::new(); n];
-    lr.sort();
-    for &(l, r) in &lr {
-        v[l - 1].push(r - 1);
+    let mut g = vec![vec![0; n + 1]; n + 1];
+    lr.iter().for_each(|&(l, r)| g[l][r] += 1);
+    for v in g.iter_mut() {
+        (1..=n).for_each(|j| v[j] += v[j - 1]);
     }
     for &(p, q) in &pq {
-        let mut answer = 0;
-        for v in &v[p - 1..q] {
-            answer += match v.binary_search(&(q - 1)) {
-                Ok(j) => j + 1,
-                Err(j) => j,
-            }
-        }
-        println!("{}", answer);
+        println!("{}", g[p..=q].iter().map(|v| v[q] - v[p - 1]).sum::<u32>());
     }
 }
