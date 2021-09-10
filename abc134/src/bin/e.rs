093c8c45e84de2b7684c0d8903926243fed14027
sugyan <sugi1982@gmail.com>
2021-09-10 23:39:04 +0900

    Update abc134/e

--- abc134/src/bin/e.rs
+++ abc134/src/bin/e.rs
@@ -1,5 +1,4 @@
 use proconio::{fastout, input};
-use std::cmp::Ordering;
 
 #[fastout]
 fn main() {
@@ -8,20 +7,12 @@ fn main() {
         a: [u32; n],
     }
     let mut v = Vec::new();
-    for &a in &a {
-        if let Err(i) = v.binary_search_by(|&probe| {
-            if probe < a {
-                Ordering::Less
-            } else {
-                Ordering::Greater
-            }
-        }) {
-            if i == 0 {
-                v.insert(0, a)
-            } else {
-                v[i - 1] = a
-            }
-        };
+    for &a in a.iter().rev() {
+        match v.binary_search(&(a + 1)) {
+            Ok(i) => v[i] = a,
+            Err(i) if i < v.len() => v[i] = a,
+            _ => v.push(a),
+        }
     }
     println!("{}", v.len());
 }
