2d6f2a736dce0c83370665ac551c684dfc638380
sugyan <sugi1982@gmail.com>
2021-09-12 23:26:59 +0900

    Update abc190/d

--- abc190/src/bin/d.rs
+++ abc190/src/bin/d.rs
@@ -3,16 +3,13 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: u64,
+        mut n: u64,
     }
-    let mut answer = 0;
-    for i in 1..=((n as f64).sqrt() as u64) {
-        if n % i == 0 {
-            if i & 1 == 0 && (n / i) & 1 == 0 {
-                continue;
-            }
-            answer += if i * i == n { 1 } else { 2 };
-        }
-    }
-    println!("{}", answer * if n & 1 == 0 { 1 } else { 2 });
+    n *= 2;
+    let answer = (1..)
+        .take_while(|&i| i * i <= n)
+        .filter(|&i| n % i == 0 && i & 1 != (n / i) & 1)
+        .count()
+        * 2;
+    println!("{}", answer);
 }
