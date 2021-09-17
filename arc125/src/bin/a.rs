92ec255f6e437293bc5c89049f165067e6a83000
sugyan <sugi1982@gmail.com>
2021-09-17 23:39:16 +0900

    Update arc125/a

--- arc125/src/bin/a.rs
+++ arc125/src/bin/a.rs
@@ -9,20 +9,12 @@ fn main() {
     }
     let answer = if t.iter().all(|&u| u == s[0]) {
         m as i32
+    } else if let Some(l) = s.iter().position(|&u| u != s[0]) {
+        (l.min(n - s.iter().rposition(|&u| u != s[0]).unwrap()) + m
+            - if s[0] != t[0] { 0 } else { 1 }
+            + t.windows(2).filter(|w| w[0] != w[1]).count()) as i32
     } else {
-        match (
-            s.iter().position(|&u| u != s[0]),
-            s.iter().rposition(|&u| u != s[0]),
-        ) {
-            (Some(l), Some(r)) => {
-                (l.min(n - r)
-                    + m
-                    + if s[0] != t[0] { 1 } else { 0 }
-                    + t.windows(2).filter(|w| w[0] != w[1]).count()
-                    - 1) as i32
-            }
-            _ => -1,
-        }
+        -1
     };
     println!("{}", answer);
 }
