9a75b6ed36b7a7cfb27c6f26e927bb1e79df90e3
sugyan <sugi1982@gmail.com>
2021-09-19 23:43:44 +0900

    Update abc216/d

--- abc216/src/bin/d.rs
+++ abc216/src/bin/d.rs
@@ -4,34 +4,23 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         mut n: usize, m: usize,
+        a: [[usize]; m],
     }
-    let mut v = Vec::new();
     let mut map = vec![Vec::new(); n];
     for i in 0..m {
-        input! {
-            k: usize,
-            a: [usize; k],
-        }
-        map[a[0] - 1].push(i);
-        v.push(a);
+        map[a[i][0] - 1].push(i);
     }
     let mut idx = vec![0; m];
-    let mut stack = (0..n)
-        .filter_map(|i| {
-            if map[i].len() > 1 {
-                Some([map[i][0], map[i][1]])
-            } else {
-                None
-            }
-        })
-        .collect::<Vec<_>>();
+    let mut stack = (0..n).filter(|&i| map[i].len() > 1).collect::<Vec<_>>();
     while let Some(last) = stack.pop() {
-        for &i in &last {
+        let v = map[last].clone();
+        for &i in &v {
             idx[i] += 1;
-            if idx[i] < v[i].len() {
-                map[v[i][idx[i]] - 1].push(i);
-                if map[v[i][idx[i]] - 1].len() > 1 {
-                    stack.push([map[v[i][idx[i]] - 1][0], map[v[i][idx[i]] - 1][1]]);
+            if idx[i] < a[i].len() {
+                let j = a[i][idx[i]] - 1;
+                map[j].push(i);
+                if map[j].len() > 1 {
+                    stack.push(j);
                 }
             }
         }
