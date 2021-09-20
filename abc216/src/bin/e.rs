4293619a97925025ce97e002a8226283113f684e
sugyan <sugi1982@gmail.com>
2021-09-20 23:58:18 +0900

    Update abc216/e

--- abc216/src/bin/e.rs
+++ abc216/src/bin/e.rs
@@ -8,21 +8,19 @@ fn main() {
     }
     a.sort();
     let mut p = a[n - 1];
-    for i in 0..n {
+    for (i, j) in (0..n).zip(1..) {
         p = if i < n - 1 { a[n - i - 2] } else { 0 };
-        if (a[n - i - 1] - p) * (i as i64 + 1) > k {
-            p = a[n - i - 1] - k / (i as i64 + 1);
-            k %= i as i64 + 1;
+        if (a[n - i - 1] - p) * j > k {
+            p = a[n - i - 1] - k / j;
+            k %= j;
             break;
         } else {
-            k -= (a[n - i - 1] - p) * (i as i64 + 1);
+            k -= (a[n - i - 1] - p) * j;
         }
     }
     let mut answer = p * k;
-    for a in a {
-        if a > p {
-            answer += (a * (a + 1) - p * (p + 1)) / 2;
-        }
+    for &a in a.iter().filter(|&a| *a > p) {
+        answer += (a * (a + 1) - p * (p + 1)) / 2;
     }
     println!("{}", answer);
 }
