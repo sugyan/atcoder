bca1d16c087ee6db02b45aa97107b892ecfe2344
sugyan <sugi1982@gmail.com>
2021-09-25 23:12:36 +0900

    Update abc135/d

--- abc135/src/bin/d.rs
+++ abc135/src/bin/d.rs
@@ -1,6 +1,6 @@
 use proconio::{fastout, input};
 
-const MOD: u32 = 1_000_000_007;
+const MOD: u64 = 1_000_000_007;
 
 #[fastout]
 fn main() {
@@ -12,13 +12,12 @@ fn main() {
     for (i, b) in s.bytes().enumerate() {
         for j in 0..13 {
             for k in 0..10 {
-                let next = (j * 10 + k as usize) % 13;
                 if b == b'?' || b - b'0' == k {
-                    dp[i + 1][next] += dp[i][j];
-                    dp[i + 1][next] %= MOD;
+                    dp[i + 1][(j * 10 + k as usize) % 13] += dp[i][j];
                 }
             }
         }
+        (0..13).for_each(|j| dp[i + 1][j] %= MOD);
     }
     println!("{}", dp[s.len()][5]);
 }
