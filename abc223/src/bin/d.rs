36c22111e4a6d20ac9bc694e4af04546000dc164
sugyan <sugi1982@gmail.com>
2021-10-25 21:34:30 +0900

    Update abc223/d

--- abc223/src/bin/d.rs
+++ abc223/src/bin/d.rs
@@ -13,16 +13,13 @@ fn main() {
         g[b - 1].0 += 1;
         g[a - 1].1.push(b - 1);
     }
-    g.iter_mut().for_each(|(_, v)| v.sort());
     let mut bh = BinaryHeap::new();
     for i in (0..n).filter(|&i| g[i].0 == 0) {
         bh.push(Reverse(i));
     }
-    let mut used = vec![false; n];
-    let mut answers = Vec::with_capacity(n);
+    let mut v = Vec::with_capacity(n);
     while let Some(Reverse(i)) = bh.pop() {
-        answers.push((i + 1).to_string());
-        used[i] = true;
+        v.push((i + 1).to_string());
         for &j in &g[i].1.clone() {
             g[j].0 -= 1;
             if g[j].0 == 0 {
@@ -30,14 +27,9 @@ fn main() {
             }
         }
     }
-    for i in 0..n {
-        if !used[i] && g[i].0 == 0 {
-            answers.push((i + 1).to_string());
-        }
-    }
-    if answers.len() == n {
-        println!("{}", answers.join(" "));
+    if v.len() == n {
+        println!("{}", v.join(" "));
     } else {
         println!("-1");
-    }
+    };
 }
