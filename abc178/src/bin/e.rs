b7ec89770e6b34c1ba0774f390e1f9d7d75f582e
sugyan <sugi1982@gmail.com>
2021-11-01 15:32:18 +0900

    Update abc178/e

--- abc178/src/bin/e.rs
+++ abc178/src/bin/e.rs
@@ -6,12 +6,9 @@ fn main() {
         n: usize,
         xy: [(i32, i32); n],
     }
-    let mut m = [0, std::i32::MAX, std::i32::MIN, std::i32::MAX];
-    for &(x, y) in &xy {
-        m[0] = m[0].max(x + y);
-        m[1] = m[1].min(x + y);
-        m[2] = m[2].max(x - y);
-        m[3] = m[3].min(x - y);
-    }
-    println!("{}", (m[0] - m[1]).max((m[2] - m[3]).abs()));
+    let v0 = xy.iter().map(|&(x, y)| x + y).collect::<Vec<_>>();
+    let v1 = xy.iter().map(|&(x, y)| x - y).collect::<Vec<_>>();
+    let answer = (v0.iter().max().unwrap() - v0.iter().min().unwrap())
+        .max(v1.iter().max().unwrap() - v1.iter().min().unwrap());
+    println!("{}", answer);
 }
