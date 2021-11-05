03ff31f8cbf8086f4484efa1508ae41ec54f8552
sugyan <sugi1982@gmail.com>
2021-11-05 14:50:49 +0900

    Update abc185/d

--- abc185/src/bin/d.rs
+++ abc185/src/bin/d.rs
@@ -6,14 +6,11 @@ fn main() {
         n: i32, m: usize,
         mut a: [i32; m],
     }
-    a.push(0);
-    a.push(n + 1);
+    a.extend(&[0, n + 1]);
     a.sort();
     let v = a.windows(2).map(|w| w[1] - w[0] - 1).collect::<Vec<_>>();
-    let answer = if let Some(min) = v.iter().filter(|&e| *e != 0).min() {
-        v.iter()
-            .map(|&e| e / min + if e % min > 0 { 1 } else { 0 })
-            .sum::<i32>()
+    let answer = if let Some(min) = v.iter().filter(|&e| *e > 0).min() {
+        v.iter().map(|&e| (e + min - 1) / min).sum()
     } else {
         0
     };
