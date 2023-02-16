90f9e03f1cb629ed10c522de71ab79048228019d
sugyan <sugi1982@gmail.com>
2023-02-16 15:02:45 +0900

    Add abc086/b

---
+++ abc086/b.ml
@@ -0,0 +1,10 @@
+open Base;;
+
+let n =
+  Caml.read_line () |> String.split ~on:' ' |> String.concat |> Int.of_string
+in
+List.range 1 n
+|> List.map ~f:(Fn.flip ( ** ) 2)
+|> List.exists ~f:(( = ) n)
+|> (function true -> "Yes" | false -> "No")
+|> Stdio.print_endline
