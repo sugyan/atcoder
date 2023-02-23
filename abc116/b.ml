65ca7ada48f2a1c60f44773d4d02121ed1fc42bb
sugyan <sugi1982@gmail.com>
2023-02-23 23:55:18 +0900

    Add abc116/b (OCaml)

---
+++ abc116/b.ml
@@ -0,0 +1,12 @@
+open Base;;
+
+let s = Caml.read_int () in
+let answer =
+  let f n = if n % 2 = 0 then n / 2 else (3 * n) + 1 in
+  let rec loop acc =
+    let a = List.hd_exn acc |> f in
+    if List.mem acc a ~equal then List.length acc + 1 else loop (a :: acc)
+  in
+  loop [ s ]
+in
+answer |> Int.to_string |> Stdio.print_endline
