64a363bd89a19f11c43761e9a8c9723361030a57
sugyan <sugi1982@gmail.com>
2023-02-27 11:31:39 +0900

    Add abc150/c (OCaml)

---
+++ abc150/c.ml
@@ -0,0 +1,23 @@
+open Base;;
+
+let n = Caml.read_int () in
+let p =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let q =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let answer =
+  let a =
+    let rec loop acc i =
+      if i > n then acc else loop ((i * List.hd_exn acc) :: acc) (i + 1)
+    in
+    loop [ 1 ] 1 |> List.rev |> List.to_array
+  in
+  let rec count = function
+    | [] -> 0
+    | hd :: tl -> (List.count tl ~f:(( > ) hd) * a.(List.length tl)) + count tl
+  in
+  count p - count q |> abs
+in
+answer |> Int.to_string |> Caml.print_endline
