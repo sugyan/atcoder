5e3c650273b6430824a98574ff2e3dc40b6a86d4
sugyan <sugi1982@gmail.com>
2023-03-02 10:52:06 +0900

    Add abc052/b (OCaml)

---
+++ abc052/b.ml
@@ -0,0 +1,12 @@
+open Base;;
+
+let _ = Caml.read_int () in
+let s = Caml.read_line () in
+let answer =
+  String.fold s ~init:(0, 0) ~f:(fun (acc, m) -> function
+    | 'I' -> (acc + 1, max m (acc + 1))
+    | 'D' -> (acc - 1, max m (acc - 1))
+    | _ -> assert false)
+  |> snd
+in
+answer |> Int.to_string |> Caml.print_endline
