8085eb4ded18574437c23577b23afc53fea2e2af
sugyan <sugi1982@gmail.com>
2023-03-19 15:03:41 +0900

    Add abc050/b (OCaml)

--- abc104/b.ml
+++ abc104/b.ml
@@ -5,7 +5,7 @@ let answer =
   Char.( = ) s.[0] 'A'
   && Char.is_lowercase s.[1]
   && Char.is_lowercase s.[String.length s - 1]
-  && String.count s ~f:(fun c -> Char.( = ) c 'C') = 1
+  && String.count s ~f:(Char.( = ) 'C') = 1
   && String.count s ~f:Char.is_uppercase = 2
 in
 answer |> (function true -> "AC" | false -> "WA") |> Caml.print_endline
