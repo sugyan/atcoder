aaf1627a035ba01a74251231064b9e7fb38aa3a0
sugyan <sugi1982@gmail.com>
2023-03-25 18:48:36 +0900

    Add abc097/b

---
+++ abc097/b.ml
@@ -0,0 +1,10 @@
+open Base;;
+
+let x = Caml.read_int () in
+let answer =
+  List.cartesian_product (List.range 1 32) (List.range 2 10)
+  |> List.map ~f:(fun (a, b) -> a ** b)
+  |> List.filter ~f:(( >= ) x)
+  |> List.fold ~init:0 ~f:max
+in
+answer |> Int.to_string |> Caml.print_endline
