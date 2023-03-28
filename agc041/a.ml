9f46389092fdfd0133e776b6d5b74beb1ac604da
sugyan <sugi1982@gmail.com>
2023-03-28 09:48:37 +0900

    Add agc041/a

---
+++ agc041/a.ml
@@ -0,0 +1,10 @@
+open Base;;
+
+let n, a, b =
+  Caml.Scanf.sscanf (Caml.read_line ()) "%d %d %d" (fun n a b -> (n, a, b))
+in
+let answer =
+  if (b - a) % 2 = 0 then (b - a) / 2
+  else min (a - 1) (n - b) + 1 + ((b - a - 1) / 2)
+in
+answer |> Int.to_string |> Caml.print_endline
