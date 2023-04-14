e5ee82d9e82212bf738956f341cf3b7949eddd1a
sugyan <sugi1982@gmail.com>
2023-04-14 13:48:19 +0900

    Add abc098/b

---
+++ abc098/b.ml
@@ -0,0 +1,12 @@
+open Base;;
+
+let n = Caml.read_int () in
+let s = Caml.read_line () in
+let answer =
+  let cs = List.init 26 ~f:(fun i -> Char.of_int_exn (i + 97)) in
+  let ok = List.mem ~equal:Char.equal in
+  List.init n ~f:(fun i -> String.to_list s |> Fn.flip List.split_n i)
+  |> List.map ~f:(fun (l, r) -> List.count cs ~f:(fun c -> ok l c && ok r c))
+  |> List.fold ~init:0 ~f:max
+in
+answer |> Int.to_string |> Caml.print_endline
