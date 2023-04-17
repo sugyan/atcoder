3e09676c9512ca6cd8c6b3e4ace92f238c72c1ed
sugyan <sugi1982@gmail.com>
2023-04-17 16:36:10 +0900

    Add abc072/c

---
+++ abc072/c.ml
@@ -0,0 +1,13 @@
+open Base;;
+
+let _ = Caml.read_int () in
+let a =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let answer =
+  let b = Array.create ~len:100_002 0 in
+  List.iter a ~f:(fun x ->
+      List.iter [ x; x + 1; x + 2 ] ~f:(fun i -> b.(i) <- b.(i) + 1));
+  Array.fold b ~init:0 ~f:max
+in
+answer |> Int.to_string |> Caml.print_endline
