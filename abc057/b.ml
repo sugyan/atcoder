1faf441bbdfd78bfd4db65b78d235124a9b9f4e0
sugyan <sugi1982@gmail.com>
2023-04-18 10:39:33 +0900

    Add abc057/b

---
+++ abc057/b.ml
@@ -0,0 +1,15 @@
+open Base;;
+
+let read_xy _ =
+  Caml.Scanf.sscanf (Caml.read_line ()) "%d %d" (fun x y -> (x, y))
+in
+let n, m = read_xy () in
+let ab = List.range 0 n |> List.map ~f:read_xy in
+let cd = List.range 0 m |> List.map ~f:read_xy in
+let answer =
+  List.map ab ~f:(fun (a, b) ->
+      List.mapi cd ~f:(fun i (c, d) -> (abs (a - c) + abs (b - d), i + 1))
+      |> List.stable_sort ~compare:Poly.compare
+      |> List.hd_exn |> snd)
+in
+answer |> List.map ~f:Int.to_string |> List.iter ~f:Caml.print_endline
