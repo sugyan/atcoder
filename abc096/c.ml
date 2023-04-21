019b5c101ddc01001eb8f495af6dfa861a99b5f5
sugyan <sugi1982@gmail.com>
2023-04-21 10:48:24 +0900

    Add abc096/c

---
+++ abc096/c.ml
@@ -0,0 +1,15 @@
+open Base;;
+
+let h, w = Caml.Scanf.sscanf (Caml.read_line ()) "%d %d" (fun h w -> (h, w)) in
+let s = List.range 0 h |> List.map ~f:(fun _ -> Caml.read_line ()) in
+let answer =
+  let s = List.to_array s in
+  let f (i, j) = i >= 0 && h > i && j >= 0 && w > j && Char.(s.(i).[j] = '#') in
+  let is_ok (i, j) =
+    Char.(s.(i).[j] = '.')
+    || List.exists [ (i - 1, j); (i, j - 1); (i + 1, j); (i, j + 1) ] ~f
+  in
+  List.cartesian_product (List.range 0 h) (List.range 0 w)
+  |> List.for_all ~f:is_ok
+in
+answer |> (function true -> "Yes" | false -> "No") |> Caml.print_endline
