9a80c38295caa290470bdfb2d85cc615f4b028e7
sugyan <sugi1982@gmail.com>
2023-04-25 10:31:58 +0900

    Add abc088/c

---
+++ abc088/c.ml
@@ -0,0 +1,19 @@
+open Base;;
+
+let c =
+  let f _ =
+    Caml.Scanf.sscanf (Caml.read_line ()) "%d %d %d" (fun c1 c2 c3 ->
+        [ c1; c2; c3 ])
+  in
+  List.range 0 3 |> List.map ~f
+in
+let answer =
+  let c = List.map c ~f:List.to_array |> List.to_array in
+  [
+    List.init 3 ~f:(fun i -> (c.(i).(1) - c.(i).(0), c.(i).(2) - c.(i).(1)));
+    List.init 3 ~f:(fun i -> (c.(1).(i) - c.(0).(i), c.(2).(i) - c.(1).(i)));
+  ]
+  |> List.for_all ~f:(fun l ->
+         List.dedup_and_sort l ~compare:Poly.compare |> List.length |> ( = ) 1)
+in
+answer |> (function true -> "Yes" | false -> "No") |> Caml.print_endline
