49a85eeb8cdd089be6b6ddfb8802063cbd0a4260
sugyan <sugi1982@gmail.com>
2023-04-28 13:53:18 +0900

    Add abc159/d

---
+++ abc159/d.ml
@@ -0,0 +1,13 @@
+open Base;;
+
+let n = Caml.read_int () in
+let a =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let answer =
+  let c = Array.create ~len:(n + 1) 0 in
+  List.iter a ~f:(fun i -> c.(i) <- c.(i) + 1);
+  let sum = Array.sum (module Int) c ~f:(fun x -> x * (x - 1) / 2) in
+  List.map a ~f:(fun i -> sum - c.(i) + 1)
+in
+answer |> List.map ~f:Int.to_string |> List.iter ~f:Caml.print_endline
