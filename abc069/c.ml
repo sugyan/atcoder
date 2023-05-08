97f93e647059152bad0110cbb821609547201382
sugyan <sugi1982@gmail.com>
2023-05-08 11:38:31 +0900

    Add abc069/c

---
+++ abc069/c.ml
@@ -0,0 +1,12 @@
+open Base;;
+
+let _ = Caml.read_int () in
+let a =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let answer =
+  let c = Array.create ~len:4 0 in
+  List.iter a ~f:(fun x -> c.(x % 4) <- c.(x % 4) + 1);
+  c.(1) + c.(3) + Bool.to_int (c.(2) > 0) <= c.(0) + 1
+in
+answer |> (function true -> "Yes" | false -> "No") |> Caml.print_endline
