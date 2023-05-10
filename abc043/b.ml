c7243315acaa687e40211fd8f7be67931aedf20a
sugyan <sugi1982@gmail.com>
2023-05-10 10:24:33 +0900

    Add abc043/b

---
+++ abc043/b.ml
@@ -0,0 +1,11 @@
+open Base;;
+
+let s = Caml.read_line () in
+let answer =
+  let f l = function
+    | 'B' -> if List.is_empty l then l else List.tl_exn l
+    | c -> c :: l
+  in
+  List.fold (String.to_list s) ~init:[] ~f |> List.rev |> String.of_char_list
+in
+answer |> Caml.print_endline
