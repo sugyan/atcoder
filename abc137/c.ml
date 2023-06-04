06ffa1fc0fba4e366ec7d646a9ea89111db08094
sugyan <sugi1982@gmail.com>
2023-06-04 22:33:42 +0900

    Add abc137/c

---
+++ abc137/c.ml
@@ -0,0 +1,13 @@
+open Base;;
+
+let n = Caml.read_int () in
+let s = List.range 0 n |> List.map ~f:(fun _ -> Caml.read_line ()) in
+let answer =
+  let f s =
+    String.to_list s |> List.sort ~compare:Char.compare |> String.of_char_list
+  in
+  let h = Hashtbl.create (module String) in
+  List.map s ~f |> List.iter ~f:(Hashtbl.incr h);
+  Hashtbl.data h |> List.sum (module Int) ~f:(fun x -> x * (x - 1) / 2)
+in
+answer |> Int.to_string |> Caml.print_endline
