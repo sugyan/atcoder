2a214f00b12963285c8971f881a56f81266b7955
sugyan <sugi1982@gmail.com>
2023-06-06 22:21:49 +0900

    Add abc089/c

---
+++ abc089/c.ml
@@ -0,0 +1,18 @@
+open Base;;
+
+let n = Caml.read_int () in
+let s = List.range 0 n |> List.map ~f:(fun _ -> Caml.read_line ()) in
+let answer =
+  let f c = List.count s ~f:(fun s -> Char.(s.[0] = c)) in
+  let a = Array.map [| 'M'; 'A'; 'R'; 'C'; 'H' |] ~f in
+  let sum = ref 0 in
+  for i = 0 to 2 do
+    for j = i + 1 to 3 do
+      for k = j + 1 to 4 do
+        sum := !sum + (a.(i) * a.(j) * a.(k))
+      done
+    done
+  done;
+  !sum
+in
+answer |> Int.to_string |> Caml.print_endline
