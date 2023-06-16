7c4277188a2e2a796bd4b1e13e42aff9a106a152
sugyan <sugi1982@gmail.com>
2023-06-16 10:35:10 +0900

    Add agc008/a

---
+++ agc008/a.ml
@@ -0,0 +1,10 @@
+open Base;;
+
+let x, y = Caml.Scanf.sscanf (Caml.read_line ()) "%d %d" (fun x y -> (x, y)) in
+let answer =
+  List.filter_map
+    [ (x, y, 0); (-x, y, 1); (x, -y, 1); (-x, -y, 2) ]
+    ~f:(fun (a, b, i) -> if a > b then None else Some (b - a + i))
+  |> List.fold ~init:Int.max_value ~f:min
+in
+answer |> Int.to_string |> Caml.print_endline
