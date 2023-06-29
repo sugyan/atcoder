b142a1c6dab8833793c6c10218e76b469925e11f
sugyan <sugi1982@gmail.com>
2023-06-29 10:24:13 +0900

    Add agc032/a

---
+++ agc032/a.ml
@@ -0,0 +1,17 @@
+open Base;;
+
+let n = Caml.read_int () in
+let b =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let answer =
+  let rec f l a =
+    List.filteri l ~f:(fun i x -> i + 1 = x) |> List.last |> function
+    | Some i ->
+        List.split_n l (i - 1) |> fun (l0, l1) ->
+        f (l0 @ List.tl_exn l1) (List.hd_exn l1 :: a)
+    | None -> if List.length a = n then a else [ -1 ]
+  in
+  f b []
+in
+answer |> List.iter ~f:(Fn.compose Caml.print_endline Int.to_string)
