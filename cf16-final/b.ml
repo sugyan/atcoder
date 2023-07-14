9b81579b7405486de7b51b71f75104105071a65d
sugyan <sugi1982@gmail.com>
2023-07-14 14:23:46 +0900

    Add cf16-final/b

---
+++ cf16-final/b.ml
@@ -0,0 +1,9 @@
+open Base;;
+
+let n = Caml.read_int () in
+let answer =
+  let rec f i = if i * (i + 1) / 2 >= n then i else f (i + 1) in
+  let i = f 1 in
+  List.range 1 (i + 1) |> List.filter ~f:(fun x -> x <> (i * (i + 1) / 2) - n)
+in
+answer |> List.iter ~f:(Fn.compose Caml.print_endline Int.to_string)
