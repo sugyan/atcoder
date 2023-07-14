d27a96dfbf9b427007c56955c74acedd3a0726a2
sugyan <sugi1982@gmail.com>
2023-07-14 14:24:13 +0900

    Add caddi2018b/c

---
+++ caddi2018b/c.ml
@@ -0,0 +1,14 @@
+open Base;;
+
+let n, p = Caml.Scanf.sscanf (Caml.read_line ()) "%d %d" (fun n p -> (n, p)) in
+let answer =
+  let rec f acc m i =
+    if i * i > m then m :: acc
+    else if m % i = 0 then f (i :: acc) (m / i) i
+    else f acc m (i + 1)
+  in
+  f [] p 2 |> List.group ~break:( <> )
+  |> List.map ~f:(fun l -> List.hd_exn l ** (List.length l / n))
+  |> List.fold ~init:1 ~f:( * )
+in
+answer |> Int.to_string |> Caml.print_endline
