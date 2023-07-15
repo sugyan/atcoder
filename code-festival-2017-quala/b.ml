dedefb8822821d1fe53fae1777cc2f5712e2d7c1
sugyan <sugi1982@gmail.com>
2023-07-15 21:42:42 +0900

    Add code-festival-2017-quala/b

---
+++ code-festival-2017-quala/b.ml
@@ -0,0 +1,11 @@
+open Base;;
+
+let n, m, k =
+  Caml.Scanf.sscanf (Caml.read_line ()) "%d %d %d" (fun n m k -> (n, m, k))
+in
+let answer =
+  List.(
+    cartesian_product (range 0 (n + 1)) (range 0 (m + 1))
+    |> exists ~f:(fun (i, j) -> (i * m) + (j * n) - (2 * i * j) = k))
+in
+answer |> (function true -> "Yes" | false -> "No") |> Caml.print_endline
