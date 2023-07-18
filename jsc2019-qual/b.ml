340afc38da1c8b6d677a147273617e14e34c619b
sugyan <sugi1982@gmail.com>
2023-07-18 21:03:34 +0900

    Add jsc2019-qual/b

---
+++ jsc2019-qual/b.ml
@@ -0,0 +1,15 @@
+open Base;;
+
+let n, k = Caml.Scanf.sscanf (Caml.read_line ()) "%d %d" (fun n k -> (n, k)) in
+let a =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let answer =
+  let a = List.to_array a in
+  let r = List.range 0 n in
+  let g ~f = List.sum (module Int) r ~f:(fun i -> List.count r ~f:(f i)) in
+  let m0 = g ~f:(fun i j -> a.(i) < a.(j)) in
+  let m1 = g ~f:(fun i j -> a.(i) < a.(j) && i > j) in
+  ((m1 * k) + (k * (k - 1) / 2 % 1_000_000_007 * m0)) % 1_000_000_007
+in
+answer |> Int.to_string |> Caml.print_endline
