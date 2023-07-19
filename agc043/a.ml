a96f35b302744e87a9a167860e37426201ea5153
sugyan <sugi1982@gmail.com>
2023-07-19 22:41:52 +0900

    Add agc043/a

---
+++ agc043/a.ml
@@ -0,0 +1,23 @@
+open Base;;
+
+let h, w = Caml.Scanf.sscanf (Caml.read_line ()) "%d %d" (fun h w -> (h, w)) in
+let s = List.range 0 h |> List.map ~f:(fun _ -> Caml.read_line ()) in
+let answer =
+  let s = List.to_array s in
+  let m = Array.make_matrix ~dimx:h ~dimy:w (h + w) in
+  m.(0).(0) <- Bool.to_int Char.(s.(0).[0] = '#');
+  for i = 0 to h - 1 do
+    for j = 0 to w - 1 do
+      let f (di, dj) =
+        if i + di >= 0 && j + dj >= 0 then
+          let d =
+            Bool.to_int Char.(s.(i + di).[j + dj] = '.' && s.(i).[j] = '#')
+          in
+          m.(i).(j) <- min m.(i).(j) (m.(i + di).(j + dj) + d)
+      in
+      List.iter [ (-1, 0); (0, -1) ] ~f
+    done
+  done;
+  m.(h - 1).(w - 1)
+in
+answer |> Int.to_string |> Caml.print_endline
