85b3c24d4efaf594f6b9103e14f85b964cc17aeb
sugyan <sugi1982@gmail.com>
2023-07-28 14:32:15 +0900

    Add abc130/d

---
+++ abc130/d.ml
@@ -0,0 +1,18 @@
+open Base;;
+
+let n, k = Caml.Scanf.sscanf (Caml.read_line ()) "%d %d" (fun n k -> (n, k)) in
+let a =
+  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+in
+let answer =
+  let b = Array.create ~len:(n + 1) 0 in
+  List.iteri a ~f:(fun i x -> b.(i + 1) <- b.(i) + x);
+  let f i =
+    Array.binary_search b ~compare `First_greater_than_or_equal_to (k + b.(i))
+    |> function
+    | Some j -> n - j + 1
+    | None -> 0
+  in
+  List.range 0 n |> List.sum (module Int) ~f
+in
+answer |> Int.to_string |> Caml.print_endline
