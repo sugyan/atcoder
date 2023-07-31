55be390d91493a614d75e6c15f38bf20ad370504
sugyan <sugi1982@gmail.com>
2023-07-31 12:46:03 +0900

    Add sumitrust2019/d

---
+++ sumitrust2019/d.ml
@@ -0,0 +1,16 @@
+open Base;;
+
+let _ = Caml.read_int () in
+let s = Caml.read_line () in
+let answer =
+  let ok pin =
+    let f acc d =
+      String.index_from s acc d
+      |> Continue_or_stop.(
+           function None -> Stop 0 | Some i -> Continue (i + 1))
+    in
+    String.to_list pin |> List.fold_until ~init:0 ~f ~finish:Fn.id |> ( <> ) 0
+  in
+  List.range 0 1000 |> List.map ~f:(Printf.sprintf "%03d") |> List.count ~f:ok
+in
+answer |> Int.to_string |> Caml.print_endline
