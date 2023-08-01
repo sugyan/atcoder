e8878b11c23f83130c9ff1b8be76fcbf92319be6
sugyan <sugi1982@gmail.com>
2023-08-01 12:25:53 +0900

    Add abc057/c

---
+++ abc057/c.ml
@@ -0,0 +1,12 @@
+open Base;;
+
+let n = Caml.read_int () in
+let answer =
+  let rec f i acc =
+    if i * i > n then acc else f (i + 1) (if n % i = 0 then i :: acc else acc)
+  in
+  let rec g m = if m = 0 then 0 else 1 + g (m / 10) in
+  f 1 []
+  |> List.fold ~init:10 ~f:(fun acc i -> max (g i) (g (n / i)) |> min acc)
+in
+answer |> Int.to_string |> Caml.print_endline
