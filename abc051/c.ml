175d33b79ae05c0e203fcc54602a48d5f27c3f76
sugyan <sugi1982@gmail.com>
2023-08-10 10:34:46 +0900

    Add abc051/c

---
+++ abc051/c.ml
@@ -0,0 +1,25 @@
+open Base;;
+
+let sx, sy, tx, ty =
+  Caml.Scanf.sscanf (Caml.read_line ()) "%d %d %d %d" (fun sx sy tx ty ->
+      (sx, sy, tx, ty))
+in
+let answer =
+  let f, dx, dy = (String.make, tx - sx, ty - sy) in
+  [
+    f dy 'U';
+    f dx 'R';
+    f dy 'D';
+    f dx 'L';
+    f 1 'L';
+    f (dy + 1) 'U';
+    f (dx + 1) 'R';
+    f 1 'D';
+    f 1 'R';
+    f (dy + 1) 'D';
+    f (dx + 1) 'L';
+    f 1 'U';
+  ]
+  |> String.concat ~sep:""
+in
+answer |> Caml.print_endline
