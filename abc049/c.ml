81f1742b6bc49e0e5b2611417a241cfdd5a02e5f
sugyan <sugi1982@gmail.com>
2023-08-17 10:52:00 +0900

    Add abc049/c

---
+++ abc049/c.ml
@@ -0,0 +1,14 @@
+open Base;;
+
+let s = Caml.read_line () in
+let answer =
+  let rec f s =
+    [ "maerd"; "remaerd"; "esare"; "resare" ]
+    |> List.find ~f:(fun p -> String.is_prefix s ~prefix:p)
+    |> function
+    | Some t -> f String.(subo s ~pos:(length t))
+    | None -> String.is_empty s
+  in
+  f (String.rev s)
+in
+answer |> (function true -> "YES" | false -> "NO") |> Caml.print_endline
