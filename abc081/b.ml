012eabac4162715e736e7574c006f532d97df28e
sugyan <sugi1982@gmail.com>
2023-09-05 13:22:42 +0900

    Update abc081/b

--- abc081/b.ml
+++ abc081/b.ml
@@ -1,11 +1,10 @@
 open Base;;
 
-let _ = Caml.read_int () in
+let _ = Stdlib.read_int () in
 let a =
-  Caml.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
+  Stdlib.read_line () |> String.split ~on:' ' |> List.map ~f:Int.of_string
 in
 let answer =
-  let rec f x = if x % 2 = 0 then 1 + f (x / 2) else 0 in
-  List.map a ~f |> List.fold ~init:64 ~f:min
+  List.map a ~f:Int.ctz |> List.min_elt ~compare |> Option.value_exn
 in
-answer |> Int.to_string |> Caml.print_endline
+answer |> Int.to_string |> Stdlib.print_endline
